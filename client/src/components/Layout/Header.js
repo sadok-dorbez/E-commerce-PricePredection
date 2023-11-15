f22a25bb54a9c7f351863c77ad9fdbd3d2019248
sadok-dorbez <sadokdorbez0@gmail.com>
2023-11-15 01:21:28 +0100

    Final I hope

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🛒 Ecommerce App
+              🛒 FashionHub
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
             vaialible
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : www.FashionHub.com
           </p>
           <p className="mt-3">
             <BiPhoneCall /> : 012-3456789
